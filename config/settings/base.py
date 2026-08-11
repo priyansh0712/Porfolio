032339e884d1fad69e2de6c2b16e65e751972b80
Ansh-Patoliya <anshpatoliya1408@gmail.com>
2026-08-11 17:04:44 +0530

    feat(settings): register TenantMiddleware in MIDDLEWARE chain

--- config/settings/base.py
+++ config/settings/base.py
@@ -60,6 +60,7 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'apps.tenants.middleware.TenantMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
